dd0fbdf2df4bdd6c2523e475abb864d0436d4b96
Joseph-Oje <josephoje247@gmail.com>
2024-05-19 23:49:19 +0000

    pagination codes

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python3
-'''Task-2's Module
-'''
+""""  1. Simple pagination  """
+
+
+from typing import Tuple
 import csv
-import math
-from typing import Tuple, List, Dict
+from math import ceil
+from typing import List
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    '''Retrives the index range from a given page and page size
-    '''
-    startIndex = (page - 1) * page_size
-    endIndex = startIndex + page_size
-    return (startIndex, endIndex)
+    """"  0. Simple helper function """
+    return (((page - 1) * page_size), (page * page_size))
 
 
 class Server:
@@ -34,29 +33,27 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        '''Retrives a page of data
-        '''
-        assert type(page) == int and type(page_size) == int
-        assert (page > 0 and page_size > 0)
-        startIndex, endIndex = index_range(page, page_size)
-        paginatedData = self.dataset()
-        if startIndex > len(paginatedData):
-            return []
-        return paginatedData[startIndex:endIndex]
+        """ 1. Simple pagination  """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
-        '''retrives a hypermedia about a page
-        '''
-        assert type(page) == int and page > 0
-        assert type(page_size) == int and page_size > 0
-        page_data = self.get_page(page, page_size)
-        total_pages = math.ceil(len(self.dataset()) / page_size)
-        page_info = {
-            'page_size': page,
-            'page': page,
-            'data': page_data,
-            'next_page': page + 1 if page + 1 < total_pages else None,
-            'prev_page': page - 1 if page > 1 else None,
-            'total_page': total_pages,
-        }
-        return page_info
+        start, end = index_range(page, page_size)
+        return self.dataset()[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """ 2. Hypermedia pagination   """
+
+        data = self.get_page(page, page_size)
+        total_pages = ceil(len(self.dataset()) / page_size)
+
+        prev_page = page - 1 if page > 1 else None
+        next_page = page + 1 if page < total_pages else None
+
+        hypermedia = {'page_size': len(data),
+                      'page': page,
+                      'data': data,
+                      'next_page': next_page,
+                      'prev_page': prev_page,
+                      'total_pages': total_pages,
+                      }
+        return hypermedia
